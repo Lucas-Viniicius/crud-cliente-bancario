b05994882bc27f6852a751284818e56d228e15d5
Lucas Vinicius <lucasprogramador77@gmail.com>
2026-03-09 23:38:30 -0300

    feat: cria entidade Cliente e adiciona dependências do JPA

---
+++ src/main/java/com/projeto/crud/model/Cliente.java
@@ -0,0 +1,23 @@
+package com.projeto.crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    
+    @Id // define essa coluna como primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera Id automaticamente
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private boolean ativo;
+    private Integer numeroDaConta;
+}
